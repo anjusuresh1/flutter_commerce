96e2426eaa1e0b3edc963ed75ec59462e7a38866
Anju Suresh <anjusuresh2001@gmail.com>
2022-04-24 16:52:31 +0530

    new

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:fluttercommerce/initializer.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 
 void main() {
-  Initializer.initialize((app) {
+  Initializer.initialize((app) async {
+    await Firebase.initializeApp();
     runApp(app);
   });
 }
